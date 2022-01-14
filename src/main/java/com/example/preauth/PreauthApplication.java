4666c355ac524e025341a9545833f4390ea3b451
한광수 <hankk20@naver.com>
2022-01-15 01:22:43 +0900

    1차

---
+++ src/main/java/com/example/preauth/PreauthApplication.java
@@ -0,0 +1,13 @@
+package com.example.preauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreauthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PreauthApplication.class, args);
+    }
+
+}
